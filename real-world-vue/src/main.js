e475dbaca15ec13fdb8a836a2d4b617c54a9d6c0
Ravi Kalia <ravkalia@gmail.com>
2018-12-12 13:33:22 -0600

    feat(svg): add svg to baseIcon

--- real-world-vue/src/main.js
+++ real-world-vue/src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import BaseIcon from "@/components/BaseIcon.v@e";
+import BaseIcon from "@/components/BaseIcon.vue";
 Vue.config.productionTip = false;
 
 Vue.component("BaseIcon", BaseIcon);
