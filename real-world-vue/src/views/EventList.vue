47fa1d740e40ace66b9371f67ff94a2786c1a879
Ravi Kalia <ravkalia@gmail.com>
2018-12-13 14:55:42 -0600

    feat(axios): add axios request to EventList

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -8,10 +8,17 @@
 
 <script>
 import EventCard from "@/components/EventCard.vue";
+import axios from "axios";
 
 export default {
   components: {
     EventCard
+  },
+  created() {
+    axios
+      .get("http://localhost:3000/events")
+      .then(response => console.log(response))
+      .catch(error => console.log(` There was an error: ${error.response}`));
   }
 };
 </script>
